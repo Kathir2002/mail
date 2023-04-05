cbc5e9d5809af7b04927431a77f2f9c751c20622
Kathir2002 <kathirmthvn@gmail.com>
2023-04-05 14:37:31 +0530

    fiinal commit

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
-const express = require("express")
-const app = express();
-const cors = require("cors")
+const express = require('express');
+const nodemailer = require('nodemailer');
 const morgan = require("morgan")
-const nodemailer = require("nodemailer")
-const env = require("dotenv")
-env.config()
-app.use(cors());
+require("dotenv").config()
+const app = express();
+const port = 3000;
+
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+
+app.post('/send-email', async (req, res) => {
+    const { subject, content } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: 'testdemomailuser@gmail.com',
+            pass: process.env.GMAIL_PASS
+        }
+    });
+
+    try {
+        await transporter.sendMail({
+            from: "testdemomailuser@gmail.com",
+            to: to,
+            subject: subject,
+            text: content
+        });
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while sending the email');
+    }
 });
-let mailTransporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "testdemomailuser@gmail.com",
-        pass: process.env.GMAIL_PASS
-    }
-})
 
-app.post("/send-email", async (req, res) => {
-    let { subject, email, content } = req.body
-    console.log(subject, content);
-    console.log(req.body);
-    let mailOptions = {
-        from: "testdemomailuser@gmail.com",
-        to: "kathirmthvn@gmail.com",
-        subject: "Your Details Changed",
-        text: "Hai"
-    }
-    mailTransporter.sendMail(mailOptions, err => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log("Email has sent");
-        }
-    })
-})
-
-app.listen(8001, () => console.log("Server Running"));
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
